f4e1419dc3d1d9f806d2677f1d2aa68fa93fccdc
Yesmin Lahoud <ymslahoud@gmail.com>
2022-09-16 14:29:58 -0300

    remocao de uuid

--- src/model/Person.ts
+++ src/model/Person.ts
@@ -1,15 +1,6 @@
-import { v4 as uuidV4 } from "uuid";
-
 class Person {
-    id?: string;
     cpf: string;
     name: string;
-
-    constructor() {
-        if (!this.id) {
-            this.id = uuidV4();
-        }
-    }
 }
 
 export { Person };
