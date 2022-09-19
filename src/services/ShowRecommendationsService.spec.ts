be9e0a2f5ef75c91bc448b35d15fe55364fb141e
Yesmin Lahoud <ymslahoud@gmail.com>
2022-09-18 21:30:41 -0300

    testes ShowRecommendationsService

---
+++ src/services/ShowRecommendationsService.spec.ts
@@ -0,0 +1,101 @@
+import { PersonsRepository } from "../repositories/implementations/PersonsRepository";
+import { RelationshipsRepository } from "../repositories/implementations/RelationshipsRepository";
+import { IPersonsRepository } from "../repositories/IPersonsRepository";
+import { IRelationshipsRepository } from "../repositories/IRelationshipsRepository";
+import { ShowRecommendationsService } from "./ShowRecommendationsService";
+
+describe("Show recommendations", () => {
+    let personsRepository: IPersonsRepository;
+    let relationshipsRepository: IRelationshipsRepository;
+    let showRecommendationsService: ShowRecommendationsService;
+
+    beforeEach(() => {
+        personsRepository = new PersonsRepository();
+        relationshipsRepository = new RelationshipsRepository();
+        showRecommendationsService = new ShowRecommendationsService(
+            personsRepository,
+            relationshipsRepository
+        );
+    });
+
+    it("Should be able to show the friends recommendations by relevance order", async () => {
+        await personsRepository.create({
+            cpf: "12345678901",
+            name: "Maria",
+        });
+
+        await personsRepository.create({
+            cpf: "12345678902",
+            name: "Pedro",
+        });
+
+        await personsRepository.create({
+            cpf: "12345678903",
+            name: "Ana",
+        });
+
+        await personsRepository.create({
+            cpf: "12345678904",
+            name: "Marcelo",
+        });
+
+        await personsRepository.create({
+            cpf: "12345678905",
+            name: "Paulo",
+        });
+
+        await relationshipsRepository.create({
+            cpf1: "12345678901",
+            cpf2: "12345678902",
+        });
+
+        await relationshipsRepository.create({
+            cpf1: "12345678901",
+            cpf2: "12345678903",
+        });
+
+        await relationshipsRepository.create({
+            cpf1: "12345678902",
+            cpf2: "12345678903",
+        });
+
+        await relationshipsRepository.create({
+            cpf1: "12345678902",
+            cpf2: "12345678904",
+        });
+
+        await relationshipsRepository.create({
+            cpf1: "12345678903",
+            cpf2: "12345678905",
+        });
+
+        await relationshipsRepository.create({
+            cpf1: "12345678903",
+            cpf2: "12345678904",
+        });
+
+        const recommendations = await showRecommendationsService.execute({
+            cpf: "12345678901",
+        });
+
+        expect(recommendations).toStrictEqual(["12345678904", "12345678905"]);
+    });
+
+    it("Should not be able to show the friends recommendations from non-existing cpf", async () => {
+        await expect(
+            showRecommendationsService.execute({
+                cpf: "12345678901",
+            })
+        ).rejects.toEqual(new Error("User not found!"));
+    });
+
+    it("Should not be able to show the friends recommendations from cpf with non-numeric characters", async () => {
+        await expect(
+            showRecommendationsService.execute({
+                cpf: "1234567890a",
+            })
+        ).rejects.toEqual(
+            new Error("The informed cpf does not have 11 numeric digits!")
+        );
+    });
+});
